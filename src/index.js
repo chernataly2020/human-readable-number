a586b072248ba6e6f8fceceb55bd9c38c27ed916
Nataliia Cherkes <69033364+chernataly2020@users.noreply.github.com>
2020-10-25 10:21:28 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+/*массив единиц*/
+const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+/*массив десятков*/
+const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+const regex = /^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/;
+
+/*Объект Number является объектом-обёрткой, позволяющей вам работать с числовыми значениями. 
+Объект Number создаётся через конструктор Number(). */
+
+const getLT20 = (n) => a[Number(n)];
+const get20Plus = (n) => b[n[0]] + ' ' + a[n[1]];
+
+module.exports = function toReadable(number) {
+    const num = Number(number)
+    if (isNaN(num)) return ''
+    if (num === 0) return 'zero'
+
+    const numStr = num.toString()
+    if (numStr.length > 9) {
+        throw new Error('overflow') // Нельзя конвертировать 10тизначное число и более
+    }
+
+    const [, n1, n2, n3, n4, n5] = ('000000000' + numStr).substr(-9).match(regex) // слева добавлены нули
+        /*формирование ответа*/
+    let str = '';
+    str += n1 != 0 ? (getLT20(n1) || get20Plus(n1)) + 'crore ' : '';
+    str += n2 != 0 ? (getLT20(n2) || get20Plus(n2)) + 'lakh ' : '';
+    str += n3 != 0 ? (getLT20(n3) || get20Plus(n3)) + 'thousand ' : '';
+    str += n4 != 0 ? getLT20(n4) + 'hundred ' : '';
+    str += n5 != 0 && str != '' ? '' : '';
+    str += n5 != 0 ? (getLT20(n5) || get20Plus(n5)) : '';
+
+    return str.trim()
 }
